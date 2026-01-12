03c481bed04d9bc57d756454f33a6781dc46fdce
Ambra <trank13f@gmail.com>
2026-01-12 08:30:48 +0100

    created class Cafe and ampl method visit_cafe to check visiters

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+import datetime
+
+
+class Cafe:
+    def __init__(self, name):
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError()
+        if visitor["vaccine"] < datetime.date.today():
+            raise OutdatedVaccineError()
+        if visitor["wearing_a_mask"] is False or "wearing_a_mask" not in visitor:
+            raise NotWearingMaskError()
+        return f"Welcome to {self.name}"
