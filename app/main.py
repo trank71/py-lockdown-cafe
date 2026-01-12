037799b2672ff2be4286045c0c4a44d318b9c53b
Ambra <trank13f@gmail.com>
2026-01-12 09:02:38 +0100

    reorgan cafe class to the sep file and created all imports from errors

--- app/main.py
+++ app/main.py
@@ -1,22 +1,3 @@
-import datetime
-
-
-class Cafe:
-    def __init__(self, name):
-        self.name = name
-
-    def visit_cafe(self, visitor: dict) -> str:
-        if "vaccine" not in visitor:
-            raise NotVaccinatedError()
-
-        expiration_date = visitor["vaccine"]["expiration_date"]
-        if expiration_date < datetime.date.today():
-            raise OutdatedVaccineError()
-
-        if not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError()
-
-        return f"Welcome to {self.name}"
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
