58ba280e1bdccd7f76d86e6b51e8cb33872b966c
Ambra <trank13f@gmail.com>
2026-01-12 09:13:56 +0100

    PEP8 Fix

--- app/cafe.py
+++ app/cafe.py
@@ -8,7 +8,7 @@ from app.errors import (
 
 
 class Cafe:
-    def __init__(self, name):
+    def __init__(self, name: str) -> None:
         self.name = name
 
     def visit_cafe(self, visitor: dict) -> str:
